662a27ce7f375dcad50528a187428ede527743f9
Ly Nguyen <lyntc.elec@gmail.com>
2023-08-13 19:18:14 +0700

    Fix click open Settings page

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,41 +1,41 @@
-import { defineManifest } from '@crxjs/vite-plugin'
+import { defineManifest } from "@crxjs/vite-plugin";
 
 export default defineManifest({
-  name: 'CreMind GPT Extension',
+  name: "CreMind GPT Extension",
   description:
-    'Unlocking the Potential of AI GPT Technology: Empowering Productivity with this Feature-Packed Extension',
-  version: '1.0.2',
+    "Unlocking the Potential of AI GPT Technology: Empowering Productivity with this Feature-Packed Extension",
+  version: "1.0.2",
   manifest_version: 3,
   icons: {
-    '16': 'img/CreMind-logo-16.png',
-    '48': 'img/CreMind-logo-48.png',
-    '128': 'img/CreMind-logo-128.png',
+    "16": "img/CreMind-logo-16.png",
+    "48": "img/CreMind-logo-48.png",
+    "128": "img/CreMind-logo-128.png",
   },
   action: {
     default_icon: {
-      '16': 'img/CreMind-logo-16.png',
-      '48': 'img/CreMind-logo-48.png',
-      '128': 'img/CreMind-logo-128.png',
+      "16": "img/CreMind-logo-16.png",
+      "48": "img/CreMind-logo-48.png",
+      "128": "img/CreMind-logo-128.png",
     },
-    default_title: 'CreMind Popup Page',
-    default_popup: 'popup.html',
+    default_title: "CreMind Popup Page",
+    default_popup: "popup.html",
   },
-  options_page: 'options.html',
+  options_page: "options.html",
   background: {
-    service_worker: 'src/background/index.ts',
-    type: 'module',
+    service_worker: "src/background/index.ts",
+    type: "module",
   },
   content_scripts: [
     {
-      matches: ['http://*/*', 'https://*/*'],
-      js: ['src/content/index.ts'],
+      matches: ["http://*/*", "https://*/*"],
+      js: ["src/content/index.ts"],
     },
   ],
   web_accessible_resources: [
     {
-      resources: ['img/*.png'],
-      matches: ['http://*/*', 'https://*/*'],
+      resources: ["img/*.png"],
+      matches: ["http://*/*", "https://*/*"],
     },
   ],
-  permissions: ['storage', 'contextMenus'],
-})
+  permissions: ["storage", "contextMenus"],
+});
